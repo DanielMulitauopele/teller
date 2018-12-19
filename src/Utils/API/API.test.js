975b26ed5a26b66baeb59f0dad8936bee531f85f
Dina <dcaraballo26@gmail.com>
2018-12-18 18:19:37 -0700

    Add fetch call to api file

--- src/Utils/API/API.test.js
+++ src/Utils/API/API.test.js
@@ -1,1 +1,16 @@
-API.test.js
+import React from 'react'
+import * as API from './'
+
+describe('API', () => {
+  it('should call fetch with the correct params', async () => {
+    const mockUrl = 'someUrl.com'
+    window.fetch = jest.fn().mockImplementation(() => 
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve()
+      })
+    )
+    await API.fetchData(mockUrl)
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+  })
+})
--- src/Utils/API/index.js
+++ src/Utils/API/index.js
@@ -1,1 +1,9 @@
-apiCalls.js
+export const fetchData = async (url) => {
+  const response = await fetch(url)
+  if (response.status >= 300) {
+    throw new Error('Fetch has failed')
+  } else {
+    const result = await response.json()
+    return result
+  }
+}
