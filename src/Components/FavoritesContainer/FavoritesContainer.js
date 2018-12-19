47bc575f35a575296cbdd06594b19af07df681a9
Nick Dambrosio <ndambro303@gmail.com>
2018-12-18 23:05:54 -0700

    add favorites slider component

--- src/Components/FavoritesContainer/FavoritesContainer.js
+++ src/Components/FavoritesContainer/FavoritesContainer.js
@@ -13,42 +13,50 @@ class FavoritesContainer extends Component {
   render() {
     var settings = {
       swipe: true,
-      fade: true,
       infinite: true,
       initialSlide: 0,
-      dots: true,
-      dotsCSS: "slick-dots",
-      arrows: true
+      buttons: false,
+      // dots: true,
+      // dotsCSS: "slick-dots",
+      arrows: false,
+      vertical: true,
+      slidesToShow: 1,
+      zoom: true,
+      magnify: true,
+      verticalSwiping: true,
+      slidesToScroll: 1
     };
     return (
-      <div className="container">
-        <Slider {...settings}>
-          <div>
-            <h5>Bitcoin</h5>
-            <h1>$4000.24</h1>
-            <h2>+$150.00 (22.1%) ^ </h2>
-          </div>
-          <div>
-            <h5>Dogecoin</h5>
-            <h1>$100.24</h1>
-            <h2>+$12.00 (22.1%) ^ </h2>
-          </div>
-          <div>
-            <h5>SomeOtherCoin</h5>
-            <h1>$3000.24</h1>
-            <h2>+$0.00 (0%) ^ </h2>
-          </div>
-          <div>
-            <h5>NickCoin</h5>
-            <h1>$2000.24</h1>
-            <h2>+$22.00 (1%) ^ </h2>
-          </div>
-          <div>
-            <h5>Etherium? that's a thing right?</h5>
-            <h1>$1000.24</h1>
-            <h2>+$23.00 (22.1%) ^ </h2>
-          </div>
-        </Slider>
+      <div className="container-box">
+        <div className="container">
+          <Slider {...settings}>
+            <div>
+              <h5>Bitcoin</h5>
+              <h1>$4000.24</h1>
+              <h2>+$150.00 (22.1%) ^ </h2>
+            </div>
+            <div>
+              <h5>Dogecoin</h5>
+              <h1>$100.24</h1>
+              <h2>+$12.00 (22.1%) ^ </h2>
+            </div>
+            <div>
+              <h5>SomeOtherCoin</h5>
+              <h1>$3000.24</h1>
+              <h2>+$0.00 (0%) ^ </h2>
+            </div>
+            <div>
+              <h5>NickCoin</h5>
+              <h1>$2000.24</h1>
+              <h2>+$22.00 (1%) ^ </h2>
+            </div>
+            <div>
+              <h5>Etherium? that's a thing right?</h5>
+              <h1>$1000.24</h1>
+              <h2>+$23.00 (22.1%) ^ </h2>
+            </div>
+          </Slider>
+        </div>
       </div>
     );
   }
