8df149615fb090b1f905030cf7d539a17dd21190
Nick Dambrosio <ndambro303@gmail.com>
2018-12-18 22:03:12 -0700

    favorite component styles

--- src/Components/FavoritesContainer/FavoritesContainer.js
+++ src/Components/FavoritesContainer/FavoritesContainer.js
@@ -23,7 +23,31 @@ class FavoritesContainer extends Component {
     return (
       <div className="container">
         <Slider {...settings}>
-          <div />
+          <div>
+            <h5>Bitcoin</h5>
+            <h1>$4000.24</h1>
+            <h2>+$150.00 (22.1%) ^ </h2>
+          </div>
+          <div>
+            <h5>Dogecoin</h5>
+            <h1>$100.24</h1>
+            <h2>+$12.00 (22.1%) ^ </h2>
+          </div>
+          <div>
+            <h5>SomeOtherCoin</h5>
+            <h1>$3000.24</h1>
+            <h2>+$0.00 (0%) ^ </h2>
+          </div>
+          <div>
+            <h5>NickCoin</h5>
+            <h1>$2000.24</h1>
+            <h2>+$22.00 (1%) ^ </h2>
+          </div>
+          <div>
+            <h5>Etherium? that's a thing right?</h5>
+            <h1>$1000.24</h1>
+            <h2>+$23.00 (22.1%) ^ </h2>
+          </div>
         </Slider>
       </div>
     );
