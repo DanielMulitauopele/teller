2eb0e8864fa7403f96248a46cbec2572b6e67260
Dina <dcaraballo26@gmail.com>
2018-12-18 19:38:48 -0700

    Add test for data cleaner

--- src/Utils/Cleaners/index.js
+++ src/Utils/Cleaners/index.js
@@ -1,1 +1,15 @@
-DataCleaner.js
+import * as API from '../API/'
+
+class DataCleaner {
+  constructor() {
+    this.fetchData = API.fetchData
+  }
+
+  getCurrencies = async () => {
+    const currencyData = await this.fetchData()
+    console.log(currencyData)
+    return currencyData
+  }
+}
+
+export default DataCleaner
