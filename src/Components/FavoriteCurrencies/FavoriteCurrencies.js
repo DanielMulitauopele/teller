df5adcf37b3add8e1a7293746477778d1b150167
Nick Dambrosio <ndambro303@gmail.com>
2018-12-20 09:27:21 -0700

    add bubbles to favorites component

--- src/Components/FavoriteCurrencies/FavoriteCurrencies.js
+++ src/Components/FavoriteCurrencies/FavoriteCurrencies.js
@@ -5,17 +5,25 @@ import Green from "../../Assets/green.svg";
 const FavoriteCurrencies = () => {
   return (
     <div className="fave-currency">
-      {/* <p>BTC</p> */}
-      <p>BITCOIN</p>
+      <div className="bubbles bubbles-left">
+        <div />
+        <div />
+        <div />
+      </div>
+      <p className="fave-name">BITCOIN</p>
       <p class="fave-price">
         $9000.<span>00</span>
       </p>
+      <div className="bubbles bubbles-right">
+        <div />
+        <div />
+        <div />
+      </div>
       <div className="price-progress">
         <p>
           +$150.55 (15% <img className="arrow" src={Green} />)
         </p>
       </div>
-      {/* <button>removeFavorites</button> */}
     </div>
   );
 };
