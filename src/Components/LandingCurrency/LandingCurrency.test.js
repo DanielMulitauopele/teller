581234420d77977bc7a1dcfd4bb59af5f3ddea9f
Dina <dcaraballo26@gmail.com>
2018-12-30 01:51:30 -0700

    Add passing tests to Landing Currency component

--- src/Components/LandingCurrency/LandingCurrency.test.js
+++ src/Components/LandingCurrency/LandingCurrency.test.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import LandingCurrency from './LandingCurrency'
 import { shallow } from 'enzyme'
+import { mockAbbrevCurrencies } from '../../Utils/MockData/mockAbbrevCurrencies'
 
 describe('LandingCurrency Component', () => {
   let wrapper
+  let i = 1
 
   beforeEach(() => {
-    wrapper = shallow(<LandingCurrency />)
+    wrapper = shallow(<LandingCurrency
+                        index={i}
+                        currency={mockAbbrevCurrencies[1]}
+                      />)
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should start with the correct default state', () => {
+    expect(wrapper.state().expanded).toEqual(false)
+  })
+
+  describe('expand function', () => {
+    it('should toggle the expanded property in state', () => {
+      const currencyCard = wrapper.find('.currency-card')
+      currencyCard.simulate('click')
+      expect(wrapper.state().expanded).toEqual(true)
+    })
   })
 })
