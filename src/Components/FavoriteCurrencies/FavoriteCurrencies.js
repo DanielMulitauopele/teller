9df52f6e4d47816d3a06343123041c197f8431cd
Dina <dcaraballo26@gmail.com>
2019-01-01 17:51:42 -0700

    Refactor FavoriteCurrencies component to change bubble speed

--- src/Components/FavoriteCurrencies/FavoriteCurrencies.js
+++ src/Components/FavoriteCurrencies/FavoriteCurrencies.js
@@ -13,23 +13,67 @@ class FavoriteCurrencies extends Component {
     super(props)
   }
 
+  controlBubbleSpeed = (percent_change) => {
+    let className = ""
+    switch (percent_change) {
+      case (percent_change >= 3):
+        className="speed-up-4"
+        //direction up
+        //all speeds divided by 4
+        break
+      case (2 <= percent_change < 3):
+        className="speed-up-3"
+        //direction up
+        //all speeds divided by 3
+        break
+      case (1 <= percent_change < 2):
+        className="speed-up-2"
+        //direction up
+        //all speeds divided by 2
+        break
+      case (0 <= percent_change < 1):
+        //everything stays how it is currently in the css file
+        break
+      case (-1 <= percent_change < 0):
+        className="down"
+        //direction down
+        break
+      case (-2 <= percent_change < -1):
+        className="speed-down-2"
+        //direction down
+        //all speeds divided by 2
+        break
+      case (-3 <= percent_change < -2):
+        className="speed-down-3"
+        //direction down
+        //all speeds divided by 3
+        break
+      case (percent_change <= -3):
+        className="speed-down-4"
+        //direction down
+        //all speeds divided by 4
+        break
+    }
+    return className
+  }
+
   render() {
     const { favorite, removeFromFavorites } = this.props
     return (
       <div className="fave-currency">
         <div className="bubbles bubbles-left">
-          <div />
-          <div />
-          <div />
+          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
         </div>
         <p className="fave-name">{favorite.name}</p>
         <p className="fave-price">$
           {favorite.price}
         </p>
         <div className="bubbles bubbles-right">
-          <div />
-          <div />
-          <div />
+          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
         </div>
         <div className="price-progress">
           <p>
