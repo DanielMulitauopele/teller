b09fefba5c155b82a18a6052ba37705950b480e9
Dina <dcaraballo26@gmail.com>
2019-01-01 18:54:13 -0700

    Refactor to change bubble speed

--- src/Components/FavoriteCurrencies/FavoriteCurrencies.js
+++ src/Components/FavoriteCurrencies/FavoriteCurrencies.js
@@ -15,44 +15,47 @@ class FavoriteCurrencies extends Component {
 
   controlBubbleSpeed = (percent_change) => {
     let className = ""
-    switch (percent_change) {
-      case (percent_change >= 3):
-        className="speed-up-4"
+    switch (true) {
+      case percent_change >= 3:
+        className = "speed-up-4"
         //direction up
         //all speeds divided by 4
         break
-      case (2 <= percent_change < 3):
-        className="speed-up-3"
+      case 2 <= percent_change < 3:
+        className = "speed-up-3"
         //direction up
         //all speeds divided by 3
         break
-      case (1 <= percent_change < 2):
-        className="speed-up-2"
+      case 1 <= percent_change < 2:
+        className = "speed-up-2"
         //direction up
         //all speeds divided by 2
         break
-      case (0 <= percent_change < 1):
+      case 0 <= percent_change < 1:
+        className = ""
         //everything stays how it is currently in the css file
         break
-      case (-1 <= percent_change < 0):
-        className="down"
+      case -1 <= percent_change < 0:
+        className = "down"
         //direction down
         break
-      case (-2 <= percent_change < -1):
-        className="speed-down-2"
+      case -2 <= percent_change < -1:
+        className = "speed-down-2"
         //direction down
         //all speeds divided by 2
         break
-      case (-3 <= percent_change < -2):
-        className="speed-down-3"
+      case -3 <= percent_change < -2:
+        className = "speed-down-3"
         //direction down
         //all speeds divided by 3
         break
-      case (percent_change <= -3):
-        className="speed-down-4"
+      case percent_change <= -3:
+        className = "speed-down-4"
         //direction down
         //all speeds divided by 4
         break
+      default:
+        className = ""
     }
     return className
   }
@@ -62,22 +65,22 @@ class FavoriteCurrencies extends Component {
     return (
       <div className="fave-currency">
         <div className="bubbles bubbles-left">
-          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
-          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
-          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={this.controlBubbleSpeed(favorite.percent_change)}/>
         </div>
         <p className="fave-name">{favorite.name}</p>
         <p className="fave-price">$
           {favorite.price}
         </p>
         <div className="bubbles bubbles-right">
-          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
-          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
-          <div className={() => this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={this.controlBubbleSpeed(favorite.percent_change)}/>
+          <div className={this.controlBubbleSpeed(favorite.percent_change)}/>
         </div>
         <div className="price-progress">
           <p>
-            {favorite.percent_change < 0 ? "-": "+"}${-1 * Math.round(favorite.price * favorite.percent_change * 100)/100} ({favorite.percent_change}% <img className="arrow" src={Green} />)
+            {favorite.percent_change < 0 ? "-": "+"}${(favorite.percent_change < 0 ? -1 : 1) * Math.round(favorite.price * favorite.percent_change)/100} ({favorite.percent_change}% <img className="arrow" src={Green} />)
           </p>
         </div>
       </div>
