e788d32c360750e5c53aa27ee948e9f5a28f51dd
Nick DAmbrosio <34214595+30ozSteak@users.noreply.github.com>
2019-01-02 21:39:14 -0700

    Routing (#58)
    * really dumb autocomplete hack implemented to remove yellowbackgrounds from inputs
    
    * change api routes in fetch
    
    * finish notes container
    
    * fix z-indexing
    
    * finish notes component

--- src/Components/NoteForm/NoteForm.js
+++ src/Components/NoteForm/NoteForm.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react'
-import './NoteForm.css'
+import React, { Component } from "react";
+import "./NoteForm.css";
 
 class NoteForm extends Component {
-  constructor(props){
-    super(props)
+  constructor(props, addToNotes) {
+    super(props, addToNotes);
     this.state = {
       title: "",
       body: ""
-    }
+    };
   }
 
-  handleChange = (e) => {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
-  }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    //submit note to backend
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addToNotes(this.state);
+    this.setState({ title: "", body: "" });
+  };
 
   render() {
-    const { title, body } = this.state
-    return(
-      <form onSubmit={this.handleSubmit}>
-        <input
-          className="title-input"
-          name="title"
-          value={title}
-          onChange={this.handleChange} />
-        <input
-          className="body-input"
-          name="body"
-          value={body}
-          onChange={this.handleChange} />
-        <button></button>
-      </form>
-    )
+    const { title, body } = this.state;
+    return (
+      <div className="note-form-literal">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="title-input"
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+            placeholder="Title"
+            autoComplete="off"
+          />
+          <input
+            className="body-input"
+            name="body"
+            placeholder="Body"
+            value={body}
+            onChange={this.handleChange}
+            autoComplete="off"
+          />
+          <button className="submit-note">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default NoteForm
+export default NoteForm;
--- src/Components/NotesContainer/NotesContainer.js
+++ src/Components/NotesContainer/NotesContainer.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react'
-import './NotesContainer.css'
-import Note from '../Note/Note'
+import React, { Component } from "react";
+import "./NotesContainer.css";
+import Note from "../Note/Note";
+import NoteForm from "../NoteForm/NoteForm";
 
 class NotesContainer extends Component {
   constructor({ props, notes, addToNotes, removeFromNotes }) {
-    super(props)
+    super(props);
   }
 
   render() {
-    const { notes, addToNotes, removeFromNotes } = this.props
+    const { notes, addToNotes, removeFromNotes } = this.props;
     const displayedNotes = notes.map((note, i) => {
-      return(
+      return (
         <Note
           key={i}
-          note={note} 
+          note={note}
           addToNotes={addToNotes}
           removeFromNotes={removeFromNotes}
         />
-      )
-    })
-    return(
-      <div>
-        {notes.length? displayedNotes : <p>No notes saved.</p>}
+      );
+    });
+    return (
+      <div className="notes-container-literal">
+        <div className="title-block">
+          <h1> Notes </h1>
+        </div>
+        <div className="notes-container-box">
+          <NoteForm notes={notes} addToNotes={addToNotes} />
+          {notes.length ? (
+            displayedNotes
+          ) : (
+            <p className="no-notes-saved">You currently have no notes saved.</p>
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NotesContainer
+export default NotesContainer;
