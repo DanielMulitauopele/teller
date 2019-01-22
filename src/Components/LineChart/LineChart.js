d6b606e6148a70f6702868871fcf2dbbb0444851
Nick Dambrosio <ndambro303@gmail.com>
2019-01-22 11:51:30 -0700

    remove transform scale on plotly

--- src/Components/LineChart/LineChart.js
+++ src/Components/LineChart/LineChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Plot from 'react-plotly.js';
+import Plot from "react-plotly.js";
+import "./LineChart.css";
 
 class LineChart extends Component {
   constructor() {
@@ -8,67 +9,51 @@ class LineChart extends Component {
   }
   async componentDidMount() {
     const url =
-      "https://cors-anywhere.herokuapp.com/https://teller-api.herokuapp.com/api/v1/assets/bitcoin/history?interval=h2";
+      "https://cors-anywhere.herokuapp.com/https://teller-api.herokuapp.com/api/v1/assets/bitcoin/history?interval=m30";
     const response = await fetch(url);
     const json = await response.json();
     this.setState({ data: json });
   }
   render() {
-    const allHistoryData = this.state.data
-    let x_values = []
-    let y_values = []
+    const allHistoryData = this.state.data;
+    let x_values = [];
+    let y_values = [];
     allHistoryData.forEach(data => {
-      x_values.push(data.date)
-      y_values.push(data.price)
+      x_values.push(data.date);
+      y_values.push(data.price);
     });
-    const modeBarConfig = {displayModeBar: false}
-    return ([
-      <div
-        style={{
-          width: this.state.fullWidth ? '100%' : '100%',
-        }}>
-      <section>
-        <Plot config={modeBarConfig} data={[
-          {
-            x: x_values,
-            y: y_values,
-            type: 'linear',
-            mode: 'lines',
-            marker: {color: '#794dff'},
-            fill: 'tozeroy',
-          }
-        ]} style={{width: '100%', height: '100%'}}
-          layout={
-          {
-            margin: {
-              l: 60,
-              r: 40,
-              b: 20,
-              t: 25,
-              pad: 5
-            },
-            height: 330,
-            title: 'Price History',
-            font: {color: 'white'},
-            visible: false,
-            xaxis: {title: false,
-                    showgrid: false},
-            yaxis: {title:'Price $',
-                    titlefont: {
-                    size: 16,
-                    },
-                    showgrid: false},
+    const modeBarConfig = { displayModeBar: false };
+    return [
+      <div>
+        <section>
+          <Plot
+            className="plot-box"
+            config={modeBarConfig}
+            data={[
+              {
+                x: x_values,
+                y: y_values,
+                type: "linear",
+                mode: "lines",
+                marker: { color: "#794dff" }
+              }
+            ]}
+            style={{ width: "26rem", height: "100%" }}
+            layout={{
+              height: 230,
+              font: { color: "white" },
+              visible: false,
+              xaxis: { title: false, showgrid: false },
+              yaxis: { title: false, showgrid: false },
 
-                    paper_bgcolor: 'rgba(0,0,0,0)',
-                    plot_bgcolor: 'rgba(0,0,0,0)',
-          }
-        }
-        />
-      </section>
+              paper_bgcolor: "rgba(0,0,0,0)",
+              plot_bgcolor: "rgba(0,0,0,0)"
+            }}
+          />
+        </section>
       </div>
-    ])
+    ];
   }
 }
 
-
-export default LineChart
+export default LineChart;
