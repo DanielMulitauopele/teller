4a8f66203987798f43e527a3937dc559a5143665
Nick Dambrosio <ndambro303@gmail.com>
2019-01-22 00:58:03 -0700

    remove graph title

--- src/Components/LineChart/LineChart.js
+++ src/Components/LineChart/LineChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Plot from 'react-plotly.js';
+import Plot from "react-plotly.js";
 
 class LineChart extends Component {
   constructor() {
@@ -14,61 +14,62 @@ class LineChart extends Component {
     this.setState({ data: json });
   }
   render() {
-    const allHistoryData = this.state.data
-    let x_values = []
-    let y_values = []
+    const allHistoryData = this.state.data;
+    let x_values = [];
+    let y_values = [];
     allHistoryData.forEach(data => {
-      x_values.push(data.date)
-      y_values.push(data.price)
+      x_values.push(data.date);
+      y_values.push(data.price);
     });
-    const modeBarConfig = {displayModeBar: false}
-    return ([
+    const modeBarConfig = { displayModeBar: false };
+    return [
       <div
         style={{
-          width: this.state.fullWidth ? '100%' : '100%',
-        }}>
-      <section>
-        <Plot config={modeBarConfig} data={[
-          {
-            x: x_values,
-            y: y_values,
-            type: 'linear',
-            mode: 'lines',
-            marker: {color: '#794dff'},
-            fill: 'tozeroy',
-          }
-        ]} style={{width: '100%', height: '100%'}}
-          layout={
-          {
-            margin: {
-              l: 60,
-              r: 40,
-              b: 20,
-              t: 25,
-              pad: 5
-            },
-            height: 330,
-            title: 'Price History',
-            font: {color: 'white'},
-            visible: false,
-            xaxis: {title: false,
-                    showgrid: false},
-            yaxis: {title:'Price $',
-                    titlefont: {
-                    size: 16,
-                    },
-                    showgrid: false},
+          width: this.state.fullWidth ? "100%" : "100%"
+        }}
+      >
+        <section>
+          <Plot
+            config={modeBarConfig}
+            data={[
+              {
+                x: x_values,
+                y: y_values,
+                type: "linear",
+                mode: "lines",
+                marker: { color: "#794dff" }
+                // fill: "tozeroy"
+              }
+            ]}
+            style={{ width: "100%", height: "100%" }}
+            layout={{
+              margin: {
+                l: 60,
+                r: 40,
+                b: 20,
+                t: 25,
+                pad: 5
+              },
+              height: 330,
+              font: { color: "white" },
+              visible: false,
+              xaxis: { title: false, showgrid: false },
+              yaxis: {
+                title: "Price $",
+                titlefont: {
+                  size: 16
+                },
+                showgrid: false
+              },
 
-                    paper_bgcolor: 'rgba(0,0,0,0)',
-                    plot_bgcolor: 'rgba(0,0,0,0)',
-          }
-        }
-        />
-      </section>
+              paper_bgcolor: "rgba(0,0,0,0)",
+              plot_bgcolor: "rgba(0,0,0,0)"
+            }}
+          />
+        </section>
       </div>
-    ])
+    ];
   }
 }
 
-
-export default LineChart
+export default LineChart;
